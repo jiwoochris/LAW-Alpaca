b3d0c28baa2a20fcf6d882598dec14ab5c9be219
Jiwoo Chris Jung <travelandi01@gmail.com>
2023-12-20 13:46:52 +0900

    Create fine-tuning.sh

---
+++ fine-tuning.sh
@@ -0,0 +1,21 @@
+python finetune.py \
+    --base_model hyunseoki/ko-en-llama2-13b \
+    --data-path jiwoochris/easylaw_kr \
+    --output_dir ./lawbot \
+    --batch_size 16 \
+    --micro_batch_size 8 \
+    --num_epochs 10 \
+    --learning_rate 0.0004 \
+    --cutoff_len 2048 \
+    --val_set_size 100 \
+    --eval_steps 10 \
+    --lora_r 16 \
+    --lora_alpha 16 \
+    --lora_dropout 0.05 \
+    --lora_target_modules '[gate_proj, down_proj, up_proj]' \
+    --train_on_inputs False \
+    --add_eos_token False \
+    --group_by_length False \
+    --prompt_template_name alpaca \
+    --lr_scheduler 'cosine' \
+    --warmup_steps 100
